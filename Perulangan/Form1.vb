e130b66c26eb13baf273ae29d92cc2e37375e3db
harllanyollanda <harllanyolland@gmail.com>
2015-10-17 22:22:20 +0700

    tugas-2015-03_Harllanyolland

--- Perulangan/Form1.vb
+++ Perulangan/Form1.vb
@@ -1,3 +1,16 @@
 ﻿Public Class Form1
 
+    Private Sub ProblemBintangToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProblemBintangToolStripMenuItem.Click
+        Bintang.MdiParent = Me
+        Bintang.Show()
+    End Sub
+
+    Private Sub MatriksKalkulatorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MatriksKalkulatorToolStripMenuItem.Click
+        Form3.MdiParent = Me
+        Form3.Show()
+    End Sub
+
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Environment.Exit(0)
+    End Sub
 End Class
